4417a6de6cba2932ae035020a0da2d17b1806986
Алексей Папин <aleksey.papin@e-legion.com>
2020-10-03 00:03:37 +0400

    removed Kingfisher, added ImagesLoader

---
+++ AlarTest/Common/Extensions/UIImage.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage.swift
+//  AlarTest
+//
+//  Created by Алексей Папин on 02.10.2020.
+//
+
+import Foundation
---
+++ AlarTest/CoreLayer/Network/ImagesLoader.swift
@@ -0,0 +1,8 @@
+//
+//  ImagesLoader.swift
+//  AlarTest
+//
+//  Created by Алексей Папин on 02.10.2020.
+//
+
+import Foundation
